9755deb6f52a3da67ba96a08a57ea998869f1f49
zzpengg <cfps91152@gmail.com>
2025-02-18 07:50:32 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    let skills = ['HTML', 'CSS', 'JS', 'React', 'NodeJS'];
+    let member = {
+      name: 'John',
+      age: 25,
+      skills: skills
+    };
+	
+}
+    
